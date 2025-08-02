07760e543359b9de293742c2a96e56637409bdf1
Loy Hui Chien <132521921+kardios@users.noreply.github.com>
2025-08-02 20:41:49 +0800

    Update app.py

--- app.py
+++ app.py
@@ -9,54 +9,31 @@ st.set_page_config(layout="wide")
 
 st.title("BattleScape")
 
-# Hardcoded data for historical battles, now with a Battle_Type column
-# We're using a string in CSV format and reading it with pandas
-battle_data = """Battle,Year,Latitude,Longitude,War,Participants,Battle_Type
-Battle of Hastings,1066,50.912,-0.488,Norman conquest of England,"Normans, English",Pitched Battle
-Battle of Waterloo,1815,50.678,4.406,Napoleonic Wars,"French Empire, Seventh Coalition",Pitched Battle
-Battle of Gettysburg,1863,39.814,-77.232,American Civil War,"United States, Confederate States",Pitched Battle
-Battle of Antietam,1862,39.473,-77.749,American Civil War,"United States, Confederate States",Pitched Battle
-Battle of Shiloh,1862,35.149,-88.322,American Civil War,"United States, Confederate States",Pitched Battle
-Siege of Vicksburg,1863,32.339,-90.878,American Civil War,"United States, Confederate States",Siege
-Battle of Stalingrad,1942,48.700,44.517,World War II,"Soviet Union, Nazi Germany",Siege
-Battle of Trafalgar,1805,36.250,-6.200,Napoleonic Wars,"United Kingdom, French and Spanish Navies",Naval
-Battle of Agincourt,1415,50.463,2.141,Hundred Years' War,"England, France",Pitched Battle
-Battle of Thermopylae,-480,38.795,22.561,Greco-Persian Wars,"Greek city-states, Persian Empire",Pitched Battle
-Battle of Salamis,-480,37.952,23.568,Greco-Persian Wars,"Greek city-states, Persian Empire",Naval
-Battle of Plataea,-479,38.216,23.279,Greco-Persian Wars,"Greek city-states, Persian Empire",Pitched Battle
-Battle of Midway,1942,28.208,-177.375,World War II,"United States, Empire of Japan",Naval
-Battle of Austerlitz,1805,49.150,16.767,Napoleonic Wars,"French Empire, Russian & Austrian Empires",Pitched Battle
-Battle of Cannae,-216,41.295,16.152,Second Punic War,"Carthage, Roman Republic",Pitched Battle
-Battle of Zama,-202,36.08,9.45,Second Punic War,"Roman Republic, Carthage",Pitched Battle
-Battle of Alesia,-52,47.53,4.49,Gallic Wars,"Roman Republic, Gallic Tribes",Siege
-Battle of Tours,732,47.394,0.685,Umayyad invasion of Gaul,"Frankish Kingdom, Umayyad Caliphate",Pitched Battle
-Battle of Yorktown,1781,37.239,-76.510,American Revolutionary War,"United States & France, Great Britain",Siege
-Battle of Marathon,-490,38.118,23.947,Greco-Persian Wars,"Athens, Persian Empire",Pitched Battle
-Battle of Actium,-31,38.955,20.766,Final War of the Roman Republic,"Octavian, Mark Antony & Cleopatra",Naval
-Battle of Dien Bien Phu,1954,21.383,103.017,First Indochina War,"Viet Minh, French Union",Siege
-Battle of Mang Yang Pass,1954,14.01,108.45,First Indochina War,"French Union, Viet Minh",Guerilla Action
-Battle of Ia Drang,1965,13.65,107.68,Vietnam War,"United States & South Vietnam, North Vietnam",Pitched Battle
-Battle of Khe Sanh,1968,16.62,106.72,Vietnam War,"United States & South Vietnam, North Vietnam",Siege
-Battle of Hue,1968,16.46,107.59,Vietnam War,"United States & South Vietnam, North Vietnam",Pitched Battle
-Battle of Sekigahara,1600,35.361,136.467,Sengoku period,"Tokugawa Ieyasu, Ishida Mitsunari",Pitched Battle
-Battle of Okehazama,1560,35.06,136.95,Sengoku period,"Oda clan, Imagawa clan",Pitched Battle
-Battle of Nagashino,1575,34.97,137.69,Sengoku period,"Oda-Tokugawa alliance, Takeda clan",Pitched Battle
-4th Battle of Kawanakajima,1561,36.58,138.18,Sengoku period,"Takeda clan, Uesugi clan",Pitched Battle
-Battle of the Somme,1916,49.99,2.65,World War I,"Allied Powers, Central Powers",Pitched Battle
-Battle of Verdun,1916,49.16,5.38,World War I,"France, German Empire",Pitched Battle
-Battle of Gallipoli,1915,40.24,26.28,World War I,"Ottoman Empire, Allied Powers",Amphibious Assault
-Siege of Jerusalem,1099,31.77,35.22,First Crusade,"Crusaders, Fatimid Caliphate",Siege
-Battle of Hattin,1187,32.79,35.44,Crusades,"Ayyubid Sultanate, Kingdom of Jerusalem",Pitched Battle
-Siege of Baghdad,1258,33.34,44.40,Mongol Invasions,"Mongol Empire, Abbasid Caliphate",Siege
-Battle of Legnica,1241,51.15,16.16,Mongol Invasions,"Mongol Empire, European Alliance",Pitched Battle
-Battle of Red Cliffs,208,29.89,113.60,Three Kingdoms Period,"Sun-Liu alliance, Cao Cao",Naval
-Battle of Ayacucho,1824,-13.16,-74.22,Peruvian War of Independence,"Peruvian & Gran Colombian forces, Spanish Empire",Pitched Battle
-Battle of Isandlwana,1879,-28.35,30.65,Anglo-Zulu War,"Zulu Kingdom, British Empire",Pitched Battle
-Castle Hill Rebellion,1804,-33.74,150.96,Australian colonial history,"Irish convicts, British colonial forces",Guerilla Action
+# Hardcoded data for historical battles, now with the full, ideal data structure.
+# We're using a string in CSV format and reading it with pandas.
+# Note: Using '|' as a separator because some descriptions contain commas.
+battle_data = """Battle|Year|Latitude|Longitude|War|Battle_Type|Description|Belligerents_A|Belligerents_B|Commanders_A|Commanders_B|Result|Wiki_URL
+Battle of Hastings|1066|50.912|-0.488|Norman conquest of England|Pitched Battle|Decisive Norman victory that led to the conquest of England.|Normans|English|William the Conqueror|King Harold Godwinson|Decisive Norman victory|https://en.wikipedia.org/wiki/Battle_of_Hastings
+Battle of Waterloo|1815|50.678|4.406|Napoleonic Wars|Pitched Battle|Final defeat of Napoleon Bonaparte, ending his rule as Emperor of the French.|United Kingdom, Prussia, Netherlands|French Empire|Duke of Wellington, Blücher|Napoleon Bonaparte|Decisive Coalition victory|https://en.wikipedia.org/wiki/Battle_of_Waterloo
+Battle of Gettysburg|1863|39.814|-77.232|American Civil War|Pitched Battle|Turning point of the American Civil War, ending Lee's invasion of the North.|United States|Confederate States|George Meade|Robert E. Lee|Decisive Union victory|https://en.wikipedia.org/wiki/Battle_of_Gettysburg
+Siege of Vicksburg|1863|32.339|-90.878|American Civil War|Siege|Gave the Union control of the Mississippi River, a critical strategic goal.|United States|Confederate States|Ulysses S. Grant|John C. Pemberton|Decisive Union victory|https://en.wikipedia.org/wiki/Siege_of_Vicksburg
+Battle of Stalingrad|1942|48.700|44.517|World War II|Siege|Major turning point on the Eastern Front of World War II.|Soviet Union|Nazi Germany, Romania, Italy|Georgy Zhukov|Friedrich Paulus|Decisive Soviet victory|https://en.wikipedia.org/wiki/Battle_of_Stalingrad
+Battle of Trafalgar|1805|36.250|-6.200|Napoleonic Wars|Naval|Ensured British naval supremacy for over a century.|United Kingdom|French Empire, Spanish Empire|Horatio Nelson|Pierre-Charles Villeneuve|Decisive British victory|https://en.wikipedia.org/wiki/Battle_of_Trafalgar
+Battle of Salamis|-480|37.952|23.568|Greco-Persian Wars|Naval|Decisive victory for the outnumbered Greek fleet, a turning point in the Persian Wars.|Greek city-states|Persian Empire|Themistocles|Xerxes I|Decisive Greek victory|https://en.wikipedia.org/wiki/Battle_of_Salamis
+Battle of Midway|1942|28.208|-177.375|World War II|Naval|Turning point in the Pacific Theater of World War II.|United States|Empire of Japan|Frank J. Fletcher, Raymond A. Spruance|Chūichi Nagumo, Nobutake Kondō|Decisive American victory|https://en.wikipedia.org/wiki/Battle_of_Midway
+Battle of Alesia|-52|47.53|4.49|Gallic Wars|Siege|Decisive Roman victory in the Gallic Wars, resulting in the Roman conquest of Gaul.|Roman Republic|Gallic Tribes|Julius Caesar|Vercingetorix|Decisive Roman victory|https://en.wikipedia.org/wiki/Battle_of_Alesia
+Battle of Yorktown|1781|37.239|-76.510|American Revolutionary War|Siege|The last major battle of the American Revolutionary War.|United States, France|Great Britain|George Washington, Rochambeau|Lord Cornwallis|Decisive American-French victory|https://en.wikipedia.org/wiki/Siege_of_Yorktown
+Battle of Gallipoli|1915|40.24|26.28|World War I|Amphibious Assault|A major Ottoman victory and a significant setback for the Allies.|Ottoman Empire|British Empire, France, Australia, New Zealand|Mustafa Kemal Atatürk|Ian Hamilton|Decisive Ottoman victory|https://en.wikipedia.org/wiki/Gallipoli_campaign
+Siege of Jerusalem|1099|31.77|35.22|First Crusade|Siege|Crusaders captured the city from the Fatimid Caliphate.|Crusaders|Fatimid Caliphate|Godfrey of Bouillon|Iftikhar ad-Daula|Decisive Crusader victory|https://en.wikipedia.org/wiki/Siege_of_Jerusalem_(1099)
+Siege of Baghdad|1258|33.34|44.40|Mongol Invasions|Siege|The fall of Baghdad marked the end of the Islamic Golden Age.|Mongol Empire|Abbasid Caliphate|Hulagu Khan|Al-Musta'sim|Decisive Mongol victory|https://en.wikipedia.org/wiki/Siege_of_Baghdad_(1258)
+Battle of Red Cliffs|208|29.89|113.60|Three Kingdoms Period|Naval|Decisive victory for the allied forces of Sun Quan and Liu Bei against the numerically superior forces of Cao Cao.|Sun Quan, Liu Bei|Cao Cao|Zhou Yu, Cheng Pu|Cao Cao|Decisive allied victory|https://en.wikipedia.org/wiki/Battle_of_Red_Cliffs
+Battle of Ayacucho|1824|-13.16|-74.22|Peruvian War of Independence|Pitched Battle|Decisive battle of the Peruvian War of Independence, securing the independence of Peru and much of South America.|Gran Colombia, Peru|Spanish Empire|Antonio José de Sucre|José de la Serna|Decisive Patriot victory|https://en.wikipedia.org/wiki/Battle_of_Ayacucho
+Battle of Isandlwana|1879|-28.35|30.65|Anglo-Zulu War|Pitched Battle|The first major encounter in the Anglo-Zulu War and a decisive victory for the Zulus.|Zulu Kingdom|British Empire|Ntshingwayo kaMahole|Lord Chelmsford|Decisive Zulu victory|https://en.wikipedia.org/wiki/Battle_of_Isandlwana
+Castle Hill Rebellion|1804|-33.74|150.96|Australian colonial history|Guerilla Action|A rebellion by Irish convicts in the British penal colony of New South Wales.|Irish convicts|British colonial forces|Phillip Cunningham|George Johnston|Decisive British victory|https://en.wikipedia.org/wiki/Castle_Hill_convict_rebellion
 """
 
-# Read the string data into a pandas DataFrame
-df = pd.read_csv(io.StringIO(battle_data))
+# Read the string data into a pandas DataFrame, using '|' as the separator
+df = pd.read_csv(io.StringIO(battle_data), sep='|')
 
 # --- Sidebar for Filters ---
 st.sidebar.header("Filter Battles")
@@ -117,19 +94,27 @@ if not filtered_df.empty:
         # Get the icon for the battle type, or use the default
         icon_name = icon_map.get(row['Battle_Type'], default_icon)
         
-        popup_text = f"""
-        <b>Battle:</b> {row['Battle']}<br>
-        <b>Date:</b> {row['Year']}<br>
-        <b>War:</b> {row['War']}<br>
-        <b>Type:</b> {row['Battle_Type']}<br>
-        <b>Participants:</b> {row['Participants']}
+        # Build the rich HTML for the popup
+        popup_html = f"""
+        <div style="width: 280px;">
+            <h4>{row['Battle']} ({row['Year']})</h4>
+            <p><strong>War:</strong> {row['War']}</p>
+            <p><strong>Type:</strong> {row['Battle_Type']}</p>
+            <hr>
+            <p><em>{row['Description']}</em></p>
+            <hr>
+            <p><strong>Belligerents:</strong><br>{row['Belligerents_A']} vs. {row['Belligerents_B']}</p>
+            <p><strong>Commanders:</strong><br>{row['Commanders_A']} vs. {row['Commanders_B']}</p>
+            <p><strong>Result:</strong> {row['Result']}</p>
+            <a href="{row['Wiki_URL']}" target="_blank">Learn More on Wikipedia</a>
+        </div>
         """
         
         tooltip_text = f"{row['Battle']} ({row['Year']})"
 
         folium.Marker(
             location=[row['Latitude'], row['Longitude']],
-            popup=folium.Popup(popup_text, max_width=300),
+            popup=folium.Popup(popup_html, max_width=300),
             tooltip=tooltip_text,
             icon=folium.Icon(color='darkred', icon=icon_name, prefix='fa')
         ).add_to(m)
