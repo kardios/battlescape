e379ce7ca8e011e681756fba7b70719995ddb542
Loy Hui Chien <132521921+kardios@users.noreply.github.com>
2025-08-02 20:38:58 +0800

    Update app.py

--- app.py
+++ app.py
@@ -9,50 +9,50 @@ st.set_page_config(layout="wide")
 
 st.title("BattleScape")
 
-# Hardcoded data for historical battles
+# Hardcoded data for historical battles, now with a Battle_Type column
 # We're using a string in CSV format and reading it with pandas
-battle_data = """Battle,Year,Latitude,Longitude,War,Participants
-Battle of Hastings,1066,50.912,-0.488,Norman conquest of England,"Normans, English"
-Battle of Waterloo,1815,50.678,4.406,Napoleonic Wars,"French Empire, Seventh Coalition"
-Battle of Gettysburg,1863,39.814,-77.232,American Civil War,"United States, Confederate States"
-Battle of Antietam,1862,39.473,-77.749,American Civil War,"United States, Confederate States"
-Battle of Shiloh,1862,35.149,-88.322,American Civil War,"United States, Confederate States"
-Siege of Vicksburg,1863,32.339,-90.878,American Civil War,"United States, Confederate States"
-Battle of Stalingrad,1942,48.700,44.517,World War II,"Soviet Union, Nazi Germany"
-Battle of Trafalgar,1805,36.250,-6.200,Napoleonic Wars,"United Kingdom, French and Spanish Navies"
-Battle of Agincourt,1415,50.463,2.141,Hundred Years' War,"England, France"
-Battle of Thermopylae,-480,38.795,22.561,Greco-Persian Wars,"Greek city-states, Persian Empire"
-Battle of Salamis,-480,37.952,23.568,Greco-Persian Wars,"Greek city-states, Persian Empire"
-Battle of Plataea,-479,38.216,23.279,Greco-Persian Wars,"Greek city-states, Persian Empire"
-Battle of Midway,1942,28.208,-177.375,World War II,"United States, Empire of Japan"
-Battle of Austerlitz,1805,49.150,16.767,Napoleonic Wars,"French Empire, Russian & Austrian Empires"
-Battle of Cannae,-216,41.295,16.152,Second Punic War,"Carthage, Roman Republic"
-Battle of Zama,-202,36.08,9.45,Second Punic War,"Roman Republic, Carthage"
-Battle of Alesia,-52,47.53,4.49,Gallic Wars,"Roman Republic, Gallic Tribes"
-Battle of Tours,732,47.394,0.685,Umayyad invasion of Gaul,"Frankish Kingdom, Umayyad Caliphate"
-Battle of Yorktown,1781,37.239,-76.510,American Revolutionary War,"United States & France, Great Britain"
-Battle of Marathon,-490,38.118,23.947,Greco-Persian Wars,"Athens, Persian Empire"
-Battle of Actium,-31,38.955,20.766,Final War of the Roman Republic,"Octavian, Mark Antony & Cleopatra"
-Battle of Dien Bien Phu,1954,21.383,103.017,First Indochina War,"Viet Minh, French Union"
-Battle of Mang Yang Pass,1954,14.01,108.45,First Indochina War,"French Union, Viet Minh"
-Battle of Ia Drang,1965,13.65,107.68,Vietnam War,"United States & South Vietnam, North Vietnam"
-Battle of Khe Sanh,1968,16.62,106.72,Vietnam War,"United States & South Vietnam, North Vietnam"
-Battle of Hue,1968,16.46,107.59,Vietnam War,"United States & South Vietnam, North Vietnam"
-Battle of Sekigahara,1600,35.361,136.467,Sengoku period,"Tokugawa Ieyasu, Ishida Mitsunari"
-Battle of Okehazama,1560,35.06,136.95,Sengoku period,"Oda clan, Imagawa clan"
-Battle of Nagashino,1575,34.97,137.69,Sengoku period,"Oda-Tokugawa alliance, Takeda clan"
-4th Battle of Kawanakajima,1561,36.58,138.18,Sengoku period,"Takeda clan, Uesugi clan"
-Battle of the Somme,1916,49.99,2.65,World War I,"Allied Powers, Central Powers"
-Battle of Verdun,1916,49.16,5.38,World War I,"France, German Empire"
-Battle of Gallipoli,1915,40.24,26.28,World War I,"Ottoman Empire, Allied Powers"
-Siege of Jerusalem,1099,31.77,35.22,First Crusade,"Crusaders, Fatimid Caliphate"
-Battle of Hattin,1187,32.79,35.44,Crusades,"Ayyubid Sultanate, Kingdom of Jerusalem"
-Siege of Baghdad,1258,33.34,44.40,Mongol Invasions,"Mongol Empire, Abbasid Caliphate"
-Battle of Legnica,1241,51.15,16.16,Mongol Invasions,"Mongol Empire, European Alliance"
-Battle of Red Cliffs,208,29.89,113.60,Three Kingdoms Period,"Sun-Liu alliance, Cao Cao"
-Battle of Ayacucho,1824,-13.16,-74.22,Peruvian War of Independence,"Peruvian & Gran Colombian forces, Spanish Empire"
-Battle of Isandlwana,1879,-28.35,30.65,Anglo-Zulu War,"Zulu Kingdom, British Empire"
-Castle Hill Rebellion,1804,-33.74,150.96,Australian colonial history,"Irish convicts, British colonial forces"
+battle_data = """Battle,Year,Latitude,Longitude,War,Participants,Battle_Type
+Battle of Hastings,1066,50.912,-0.488,Norman conquest of England,"Normans, English",Pitched Battle
+Battle of Waterloo,1815,50.678,4.406,Napoleonic Wars,"French Empire, Seventh Coalition",Pitched Battle
+Battle of Gettysburg,1863,39.814,-77.232,American Civil War,"United States, Confederate States",Pitched Battle
+Battle of Antietam,1862,39.473,-77.749,American Civil War,"United States, Confederate States",Pitched Battle
+Battle of Shiloh,1862,35.149,-88.322,American Civil War,"United States, Confederate States",Pitched Battle
+Siege of Vicksburg,1863,32.339,-90.878,American Civil War,"United States, Confederate States",Siege
+Battle of Stalingrad,1942,48.700,44.517,World War II,"Soviet Union, Nazi Germany",Siege
+Battle of Trafalgar,1805,36.250,-6.200,Napoleonic Wars,"United Kingdom, French and Spanish Navies",Naval
+Battle of Agincourt,1415,50.463,2.141,Hundred Years' War,"England, France",Pitched Battle
+Battle of Thermopylae,-480,38.795,22.561,Greco-Persian Wars,"Greek city-states, Persian Empire",Pitched Battle
+Battle of Salamis,-480,37.952,23.568,Greco-Persian Wars,"Greek city-states, Persian Empire",Naval
+Battle of Plataea,-479,38.216,23.279,Greco-Persian Wars,"Greek city-states, Persian Empire",Pitched Battle
+Battle of Midway,1942,28.208,-177.375,World War II,"United States, Empire of Japan",Naval
+Battle of Austerlitz,1805,49.150,16.767,Napoleonic Wars,"French Empire, Russian & Austrian Empires",Pitched Battle
+Battle of Cannae,-216,41.295,16.152,Second Punic War,"Carthage, Roman Republic",Pitched Battle
+Battle of Zama,-202,36.08,9.45,Second Punic War,"Roman Republic, Carthage",Pitched Battle
+Battle of Alesia,-52,47.53,4.49,Gallic Wars,"Roman Republic, Gallic Tribes",Siege
+Battle of Tours,732,47.394,0.685,Umayyad invasion of Gaul,"Frankish Kingdom, Umayyad Caliphate",Pitched Battle
+Battle of Yorktown,1781,37.239,-76.510,American Revolutionary War,"United States & France, Great Britain",Siege
+Battle of Marathon,-490,38.118,23.947,Greco-Persian Wars,"Athens, Persian Empire",Pitched Battle
+Battle of Actium,-31,38.955,20.766,Final War of the Roman Republic,"Octavian, Mark Antony & Cleopatra",Naval
+Battle of Dien Bien Phu,1954,21.383,103.017,First Indochina War,"Viet Minh, French Union",Siege
+Battle of Mang Yang Pass,1954,14.01,108.45,First Indochina War,"French Union, Viet Minh",Guerilla Action
+Battle of Ia Drang,1965,13.65,107.68,Vietnam War,"United States & South Vietnam, North Vietnam",Pitched Battle
+Battle of Khe Sanh,1968,16.62,106.72,Vietnam War,"United States & South Vietnam, North Vietnam",Siege
+Battle of Hue,1968,16.46,107.59,Vietnam War,"United States & South Vietnam, North Vietnam",Pitched Battle
+Battle of Sekigahara,1600,35.361,136.467,Sengoku period,"Tokugawa Ieyasu, Ishida Mitsunari",Pitched Battle
+Battle of Okehazama,1560,35.06,136.95,Sengoku period,"Oda clan, Imagawa clan",Pitched Battle
+Battle of Nagashino,1575,34.97,137.69,Sengoku period,"Oda-Tokugawa alliance, Takeda clan",Pitched Battle
+4th Battle of Kawanakajima,1561,36.58,138.18,Sengoku period,"Takeda clan, Uesugi clan",Pitched Battle
+Battle of the Somme,1916,49.99,2.65,World War I,"Allied Powers, Central Powers",Pitched Battle
+Battle of Verdun,1916,49.16,5.38,World War I,"France, German Empire",Pitched Battle
+Battle of Gallipoli,1915,40.24,26.28,World War I,"Ottoman Empire, Allied Powers",Amphibious Assault
+Siege of Jerusalem,1099,31.77,35.22,First Crusade,"Crusaders, Fatimid Caliphate",Siege
+Battle of Hattin,1187,32.79,35.44,Crusades,"Ayyubid Sultanate, Kingdom of Jerusalem",Pitched Battle
+Siege of Baghdad,1258,33.34,44.40,Mongol Invasions,"Mongol Empire, Abbasid Caliphate",Siege
+Battle of Legnica,1241,51.15,16.16,Mongol Invasions,"Mongol Empire, European Alliance",Pitched Battle
+Battle of Red Cliffs,208,29.89,113.60,Three Kingdoms Period,"Sun-Liu alliance, Cao Cao",Naval
+Battle of Ayacucho,1824,-13.16,-74.22,Peruvian War of Independence,"Peruvian & Gran Colombian forces, Spanish Empire",Pitched Battle
+Battle of Isandlwana,1879,-28.35,30.65,Anglo-Zulu War,"Zulu Kingdom, British Empire",Pitched Battle
+Castle Hill Rebellion,1804,-33.74,150.96,Australian colonial history,"Irish convicts, British colonial forces",Guerilla Action
 """
 
 # Read the string data into a pandas DataFrame
@@ -90,11 +90,19 @@ filtered_df = filtered_df[
 
 # --- Map Creation ---
 
-# Create the map using the 'CartoDB Positron' tile layer
-# The map is centered to be more dynamic based on the filtered data
+# Define a mapping from Battle_Type to a Font Awesome icon
+icon_map = {
+    'Naval': 'anchor',
+    'Siege': 'university',  # Represents a fortified building/city
+    'Pitched Battle': 'shield-alt',
+    'Guerilla Action': 'fire',
+    'Amphibious Assault': 'ship'
+}
+default_icon = 'crosshairs'
+
+# Create the map
 if not filtered_df.empty:
     map_center = [filtered_df['Latitude'].mean(), filtered_df['Longitude'].mean()]
-    # Adjust zoom based on how many unique wars are in the filtered set
     zoom_start = 5 if len(filtered_df['War'].unique()) == 1 and selected_war != 'All Wars' else 2
 else:
     map_center = [20, 0]
@@ -103,27 +111,28 @@ else:
 
 m = folium.Map(location=map_center, zoom_start=zoom_start, tiles='CartoDB Positron')
 
-# Add markers for each battle in the filtered dataframe
+# Add markers for each battle
 if not filtered_df.empty:
     for index, row in filtered_df.iterrows():
-        # Create the popup text
+        # Get the icon for the battle type, or use the default
+        icon_name = icon_map.get(row['Battle_Type'], default_icon)
+        
         popup_text = f"""
         <b>Battle:</b> {row['Battle']}<br>
         <b>Date:</b> {row['Year']}<br>
         <b>War:</b> {row['War']}<br>
+        <b>Type:</b> {row['Battle_Type']}<br>
         <b>Participants:</b> {row['Participants']}
         """
         
-        # Create the tooltip text
         tooltip_text = f"{row['Battle']} ({row['Year']})"
 
-        # Add the marker to the map with a custom icon
         folium.Marker(
             location=[row['Latitude'], row['Longitude']],
             popup=folium.Popup(popup_text, max_width=300),
             tooltip=tooltip_text,
-            icon=folium.Icon(color='darkred', icon='crosshairs', prefix='fa')
+            icon=folium.Icon(color='darkred', icon=icon_name, prefix='fa')
         ).add_to(m)
 
-# Display the map in the Streamlit app
+# Display the map
 st_data = st_folium(m, width=1200, height=800)
